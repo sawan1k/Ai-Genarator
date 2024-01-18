ec30d436e11f6bd84157ea42a05ae1f37607cf1c
sawan1k <sawankushwaha222@gmail.com>
2024-01-18 19:22:00 +0530

    folder Structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,4 @@
+
+export default function DashboardPage() {
+  return <p className="text-center text-2xl">Dashboard Page</p>;
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+  return <Button variant="destructive">Jai shree ram</Button>;
+}
