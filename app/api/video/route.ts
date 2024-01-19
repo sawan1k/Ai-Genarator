39ea9309ea4e101b48178ede1dcd7a8e2b25e9c8
sawan1k <sawankushwaha222@gmail.com>
2024-01-19 19:24:32 +0530

    video UI

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,15 +1,13 @@
+import Replicate from "replicate";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
 
-// import { checkSubscription } from "@/lib/subscription";
 // import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+// import { checkSubscription } from "@/lib/subscription";
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN!,
 });
-
-const openai = new OpenAIApi(configuration);
 
 export async function POST(
     req: Request
@@ -17,26 +15,14 @@ export async function POST(
     try {
         const { userId } = auth();
         const body = await req.json();
-        const { prompt, amount = 1, resolution = "512x512" } = body;
+        const { prompt } = body;
 
         if (!userId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
 
-        if (!configuration.apiKey) {
-            return new NextResponse("OpenAI API Key not configured.", { status: 500 });
-        }
-
         if (!prompt) {
             return new NextResponse("Prompt is required", { status: 400 });
-        }
-
-        if (!amount) {
-            return new NextResponse("Amount is required", { status: 400 });
-        }
-
-        if (!resolution) {
-            return new NextResponse("Resolution is required", { status: 400 });
         }
 
         // const freeTrial = await checkApiLimit();
@@ -46,19 +32,22 @@ export async function POST(
         //     return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
         // }
 
-        const response = await openai.createImage({
-            prompt,
-            n: parseInt(amount, 10),
-            size: resolution,
-        });
+        const response = await replicate.run(
+            "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
+            {
+                input: {
+                    prompt,
+                }
+            }
+        );
 
         // if (!isPro) {
         //     await incrementApiLimit();
         // }
 
-        return NextResponse.json(response.data.data);
+        return NextResponse.json(response);
     } catch (error) {
-        console.log('[IMAGE_ERROR]', error);
+        console.log('[VIDEO_ERROR]', error);
         return new NextResponse("Internal Error", { status: 500 });
     }
 };
